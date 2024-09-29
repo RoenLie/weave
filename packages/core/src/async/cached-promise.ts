2696dc792a898861a18d5585f033fcb6265ee225
Kristoffer Roen-Lie <KristofferRoenLie@gmail.com>
2024-09-29 17:56:21 +0200

    added cached promise helper.

---
+++ packages/core/src/async/cached-promise.ts
@@ -0,0 +1,26 @@
+/**
+ * Wraps a function that generates a `Promise`.\
+ * Will return the same promise to all callers until promise has been resolved\
+ * and flush argument was set to true.
+ */
+export const cachedPromise = <T extends (...args: any) => Promise<any>>(
+	func: T,
+	/**
+	 * Clear the cache after response has completed?
+	 * @default true
+	 */
+	flush = true,
+) => {
+	let promise: Promise<Awaited<ReturnType<T>>> | undefined;
+
+	return (...args: Parameters<T>) => {
+		if (promise)
+			return promise;
+
+		promise = func(...args);
+		if (flush)
+			promise.finally(() => promise = undefined);
+
+		return promise;
+	};
+};
